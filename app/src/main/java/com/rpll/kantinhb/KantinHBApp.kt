bca3e11945d6ceda47430985ccdfae7324fcfe17
christiansahlim <christiansahlim12@gmail.com>
2023-10-26 16:40:47 +0700

    Add MainActivity

---
+++ app/src/main/java/com/rpll/kantinhb/KantinHBApp.kt
@@ -0,0 +1,16 @@
+package com.rpll.kantinhb
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import com.rpll.kantinhb.navigation.NavigationBuilder
+
+class KantinHBApp {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            KantinhbTheme {
+//                NavigationBuilder()
+//            }
+//        }
+//    }
+}
--- app/src/main/java/com/rpll/kantinhb/SpatulaApp.kt
+++
@@ -1,4 +0,0 @@
-package com.rpll.kantinhb
-
-class SpatulaApp {
-}
