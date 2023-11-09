7aa08ce60b2d4758f3d6eacb46d933954c732b3f
christiansahlim <christiansahlim12@gmail.com>
2023-11-09 14:37:53 +0700

    Edit and Fix Cart Screen

---
+++ app/src/main/java/com/rpll/kantinhb/ui/components/TopBar.kt
@@ -0,0 +1,105 @@
+package com.rpll.kantinhb.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.rpll.kantinhb.R
+
+@Composable
+fun TopHomeBar(
+    goToDetail: () -> Unit,
+    goToCart: () -> Unit,
+    isAddedNewItem: Boolean? = false
+) {
+    TopAppBar (
+        title = {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                modifier = Modifier.height(30.dp)
+            )
+        },
+        actions = {
+            Row {
+                if (isAddedNewItem == true){
+                    IconButton(onClick = goToCart) {
+                        BadgedBox(
+                            badge = {
+                                Badge{}
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ShoppingCart,
+                                contentDescription = "Cart"
+                            )
+                        }
+                    }
+                }else {
+                    IconButton(onClick = goToCart) {
+                        Icon(
+                            imageVector = Icons.Default.ShoppingCart,
+                            contentDescription = "Cart"
+                        )
+                    }
+                }
+                IconButton(onClick = goToDetail ) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "about_page"
+                    )
+                }
+            }
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White,
+        elevation = 10.dp
+    )
+}
+@Composable
+fun TopBackBar(
+    navController: NavController,
+    currentPage: String
+) {
+    TopAppBar (
+        title = {
+            Text(
+                currentPage,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+            )
+        },
+        navigationIcon = if (navController.previousBackStackEntry != null) {
+            {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        } else {
+            null
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White,
+        elevation = 10.dp
+    )
+}
---
+++ app/src/main/java/com/rpll/kantinhb/ui/screen/category/CategoryScreen.kt
@@ -0,0 +1,252 @@
+package com.rpll.kantinhb.ui.screen.category
+
+import android.content.res.Configuration
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.google.accompanist.flowlayout.FlowRow
+import com.rpll.kantinhb.di.Injection
+import com.rpll.kantinhb.ui.ViewModelFactory
+import com.rpll.kantinhb.ui.common.UiState
+import com.rpll.kantinhb.ui.components.*
+import com.rpll.kantinhb.ui.theme.VividGreen_100
+
+@Composable
+fun CategoryScreen(
+    navController: NavController,
+    selectedCategoryId: Int,
+    viewModel: CategoryViewModel = androidx.lifecycle.viewmodel.compose.viewModel(
+        factory = ViewModelFactory(Injection.provideRepository())
+    )
+){
+    val selectedCategoryState = remember {
+        mutableStateOf(selectedCategoryId)
+    }
+
+    //Screen config
+    val configuration = LocalConfiguration.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+    ){
+        CustomTopNavigationBar(title= "Category", navController = navController)
+
+        when (configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                LandscapeVersion(viewModel, selectedCategoryState, navController)
+            }
+            else -> {
+                PortraitVersion(viewModel, selectedCategoryState, navController)
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+private fun LandscapeVersion(
+    viewModel: CategoryViewModel,
+    selectedCategoryState: MutableState<Int>,
+    navController: NavController
+) {
+
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        //Chips
+        viewModel.categoriesUiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+            when (uiState) {
+                is UiState.Loading -> {
+                    viewModel.getAllCategories()
+                }
+                is UiState.Success -> {
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        FlowRow(
+                            modifier = Modifier
+                                .padding(start = 16.dp)
+                                .testTag("CategoryChipList"),
+                            mainAxisSpacing = 8.dp,
+                            crossAxisSpacing = 8.dp,
+                        ) {
+                            (uiState.data).forEach { category ->
+                                if (category.id == selectedCategoryState.value) {
+                                    Chip(
+                                        onClick = {
+                                            viewModel.getProductByCategoryId(category.id)
+                                            selectedCategoryState.value = category.id
+                                            Log.e("CLICKED", "CHIP CLICKED: ${category.name}")
+                                        },
+                                        border = BorderStroke(
+                                            ChipDefaults.OutlinedBorderSize,
+                                            VividGreen_100
+                                        ),
+                                        colors = ChipDefaults.chipColors(
+                                            backgroundColor = Color.White,
+                                            contentColor = VividGreen_100
+                                        )
+                                    ) {
+                                        Text(category.name)
+                                    }
+                                } else {
+                                    Chip(
+                                        onClick = {
+                                            viewModel.getProductByCategoryId(category.id)
+                                            selectedCategoryState.value = category.id
+                                            Log.e("CLICKED", "CHIP CLICKED: ${category.name}")
+                                        },
+                                        colors = ChipDefaults.chipColors(
+                                            backgroundColor = VividGreen_100,
+                                            contentColor = Color.White
+                                        )
+                                    ) {
+                                        Text(category.name)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                is UiState.Error -> {}
+            }
+        }
+
+        //Products
+        viewModel.productUiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+            when (uiState) {
+                is UiState.Loading -> {
+                    viewModel.getProductByCategoryId(selectedCategoryState.value)
+                    Loader(Modifier.size(80.dp))
+                }
+                is UiState.Success -> {
+                    LazyColumn(
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                        modifier = Modifier
+                            .weight(3f)
+                            .padding(vertical = 10.dp)
+                            .testTag("productByCategoryList")
+                    ) {
+                        items(uiState.data, key = { product -> product.id }) { product ->
+                            ProductItem02(
+                                navController = navController,
+                                product = product
+                            )
+                        }
+                    }
+                }
+                is UiState.Error -> {}
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+private fun PortraitVersion(
+    viewModel: CategoryViewModel,
+    selectedCategoryState: MutableState<Int>,
+    navController: NavController
+) {
+    Column {
+        //Chips
+        viewModel.categoriesUiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+            when (uiState) {
+                is UiState.Loading -> {
+                    viewModel.getAllCategories()
+                }
+                is UiState.Success -> {
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        contentPadding = PaddingValues(horizontal = 8.dp),
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .testTag("CategoryChipList")
+                    ) {
+                        items(uiState.data) { category ->
+                            if (category.id == selectedCategoryState.value) {
+                                Chip(
+                                    onClick = {
+                                        viewModel.getProductByCategoryId(category.id)
+                                        selectedCategoryState.value = category.id
+                                    },
+                                    border = BorderStroke(
+                                        ChipDefaults.OutlinedBorderSize,
+                                        VividGreen_100
+                                    ),
+                                    colors = ChipDefaults.chipColors(
+                                        backgroundColor = Color.White,
+                                        contentColor = VividGreen_100
+                                    )
+                                ) {
+                                    Text(category.name)
+                                }
+                            } else {
+                                Chip(
+                                    onClick = {
+                                        viewModel.getProductByCategoryId(category.id)
+                                        selectedCategoryState.value = category.id
+                                    },
+                                    colors = ChipDefaults.chipColors(
+                                        backgroundColor = VividGreen_100,
+                                        contentColor = Color.White
+                                    )
+                                ) {
+                                    Text(category.name)
+                                }
+                            }
+                        }
+                    }
+                }
+                is UiState.Error -> {}
+            }
+        }
+
+        //Products
+        viewModel.productUiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+            when (uiState) {
+                is UiState.Loading -> {
+                    viewModel.getProductByCategoryId(selectedCategoryState.value)
+                    Loader(Modifier.size(80.dp))
+                }
+                is UiState.Success -> {
+                    LazyColumn(
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 10.dp)
+                            .testTag("productByCategoryList")
+                    ) {
+                        items(uiState.data, key = { product -> product.id }) { product ->
+                            ProductItem02(
+                                navController = navController,
+                                product = product
+                            )
+                        }
+                    }
+                }
+                is UiState.Error -> {}
+            }
+        }
+    }
+}
