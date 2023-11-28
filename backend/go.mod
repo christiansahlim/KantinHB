ba04fdb027fff5c014e2e6262316e0bcef63460d
adaf <adafcaefc@gmail.com>
2023-11-28 22:15:11 +0700

    added main backend

---
+++ backend/go.mod
@@ -0,0 +1,25 @@
+module KantinHB
+
+go 1.17
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-redis/redis v6.15.5+incompatible
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jasonlvhit/gocron v0.0.1
+	github.com/joho/godotenv v1.4.0
+	github.com/rs/cors v1.8.2
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gorm.io/driver/mysql v1.3.3
+	gorm.io/gorm v1.23.4
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	c "KantinHB/xcontroller"
+	"KantinHB/xhandler"
+	"KantinHB/xresponse"
+	_ "github.com/go-sql-driver/mysql" // Connection
+	"github.com/gorilla/mux"           // Router
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Load ENV
+	err := godotenv.Load()
+	xresponse.CheckError(err)
+
+	// Clear Redis Cache
+	err = c.ClearRedisCache(c.GetRedisClient())
+	xresponse.CheckError(err)
+
+	// Start Mux
+	router := mux.NewRouter()
+
+	// General Endpoint
+	router.HandleFunc("/register", c.UserRegister).Methods("POST")
+	router.HandleFunc("/login", c.UserLogin).Methods("POST")
+	router.HandleFunc("/logout", c.Logout).Methods("POST")
+
+	// User Endpoint
+	router.HandleFunc("/users", xhandler.Authenticate(c.GetUsers, true)).Methods("GET")
+	router.HandleFunc("/user", c.GetUser).Methods("GET")
+	router.HandleFunc("/user", xhandler.Authenticate(c.AddUser, true)).Methods("POST")
+	router.HandleFunc("/user", xhandler.Authenticate(c.UpdateUser, true)).Methods("PUT")
+	router.HandleFunc("/user/edit", c.UpdateUserEdit).Methods("PUT")
+	router.HandleFunc("/user", xhandler.Authenticate(c.DeleteUser, true)).Methods("DELETE")
+
+	// Item Endpoint
+	router.HandleFunc("/items", c.GetItems).Methods("GET")
+	router.HandleFunc("/item", c.GetItem).Methods("GET")
+	router.HandleFunc("/item", xhandler.Authenticate(c.AddItem, true)).Methods("POST")
+	router.HandleFunc("/item", xhandler.Authenticate(c.UpdateItem, true)).Methods("PUT")
+	router.HandleFunc("/item", xhandler.Authenticate(c.DeleteItem, true)).Methods("DELETE")
+
+	// Transaction Endpoint
+	router.HandleFunc("/transactions/user", xhandler.Authenticate(c.GetTransactionsUser, false)).Methods("GET")
+	router.HandleFunc("/transactions", xhandler.Authenticate(c.GetTransactions, true)).Methods("GET")
+	router.HandleFunc("/transaction", xhandler.Authenticate(c.GetTransaction, true)).Methods("GET")
+	router.HandleFunc("/transaction", xhandler.Authenticate(c.UpdateTransaction, true)).Methods("PUT")
+	router.HandleFunc("/transaction", xhandler.Authenticate(c.DeleteTransaction, true)).Methods("DELETE")
+
+	// Cart Endpoint
+	router.HandleFunc("/cart/items", xhandler.Authenticate(c.GetItemsFromCart, false)).Methods("GET")
+	router.HandleFunc("/cart/item", xhandler.Authenticate(c.GetItemFromCart, false)).Methods("GET")
+	router.HandleFunc("/cart/item", xhandler.Authenticate(c.AddItemToCart, false)).Methods("POST")
+	router.HandleFunc("/cart/item", xhandler.Authenticate(c.UpdateCartItem, false)).Methods("PUT")
+	router.HandleFunc("/cart/item", xhandler.Authenticate(c.DeleteCartItem, false)).Methods("DELETE")
+	router.HandleFunc("/cart/checkout", xhandler.Authenticate(c.CheckoutCart, false)).Methods("POST")
+
+	// CORS
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+	})
+
+	// Start Server
+	http.Handle("/", router)
+
+	log.Println("Starting " + os.Getenv("APP_NAME"))
+	log.Println("Connected to port 8081")
+	log.Fatal(http.ListenAndServe(":8081", corsHandler.Handler(router)))
+}
