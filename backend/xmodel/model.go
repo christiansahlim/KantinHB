7653583308009226bac54265ec785fc2e2a6217b
adaf <adafcaefc@gmail.com>
2023-11-29 23:31:15 +0700

    add category model

--- backend/xmodel/model.go
+++ backend/xmodel/model.go
@@ -10,11 +10,19 @@ type User struct {
 	Admin    bool   `json:"admin"`
 }
 
+type Category struct {
+	ID    	int    `json:"id"`
+	Name  	string `json:"name"`
+	Image 	string `json:"image"`
+}
+
 type Item struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name"`
 	Price       int    `json:"price"`
 	Description string `json:"description"`
+	Image 		string `json:"image"`
+	CategoryID 	int    `json:"category_id"`
 }
 
 type DetailedCart struct {
